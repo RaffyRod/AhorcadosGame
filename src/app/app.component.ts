4e675619fac48aec94161fd01aca274f7c2da59e
elraffy <elraffy3@gmail.com>
2021-03-17 16:04:57 -0400

    validation added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ export class AppComponent {
 
   palabra = 'AGUA';
   palabraOculta = '';
+  intentos = 0;
 
 
   letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
@@ -18,5 +19,25 @@ export class AppComponent {
 
   constructor(){
     this.palabraOculta = '_ '.repeat( this.palabra.length );
+  }
+  comprobar( letra ){
+    this.existeLetra( letra );
+    const  palabraOcultaArr = this.palabraOculta.split(' ');
+    console.log(palabraOcultaArr);
+    for( let i = 0; i < this.palabra.length; i++ ){
+       if( this.palabra[i] === letra ){
+        palabraOcultaArr[i] = letra;
+       }
+    }
+    this.palabraOculta = palabraOcultaArr.join(' ');
+  }
+
+  existeLetra( letra ){
+     if( this.palabra.indexOf( letra ) >= 0  ){
+          console.log('Letra existe: ' + letra);
+     } else {
+      console.log('Letra no existe: ' + letra);
+      this.intentos++;
+     }
   }
 }
